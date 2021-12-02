04217409aab12c75816906ee664038065c1878f6
andres981020 <mosquera.andres@correounivalle.edu.co>
2021-12-02 15:20:15 -0500

    Agregando funcionalidades

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Video } from 'src/app/models/youtube.models';
+import { YoutubeService } from 'src/app/services/youtube.service';
+
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  videos: Video[] = [];
+
+  constructor( private youtubeService: YoutubeService) { }
 
   ngOnInit(): void {
+
+    this.cargarVideos();
+    
   }
 
+  cargarVideos () {
+
+    this.youtubeService.getVideos()
+      .subscribe(res => {
+
+        this.videos .push(...res);
+
+        console.log(this.videos);
+        
+        
+      });
+
+  }
+
+  mostrarVideo( video: Video ) {
+
+    console.log(video);
+    Swal.fire({
+      html: `
+      <h4> ${ video.title } <h4>
+      <iframe width="100%" 
+        height="315" 
+        src="https://www.youtube.com/embed/${ video.resourceId.videoId }" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; 
+        autoplay; 
+        clipboard-write; 
+        encrypted-media; 
+        gyroscope; 
+        picture-in-picture" 
+        allowfullscreen>
+      </iframe>
+      `
+    })
+    
+  }
 }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { YoutubeResponse } from '../models/youtube.models';
+import { map } from 'rxjs/operators'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyBZ5VgZQqnIWMAc3TAlnS-tJ-KdvHltLzU';
+  private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextToken = 'EAAaBlBUOkNBVQ';
+
+  constructor( private http: HttpClient) {
+
+  }
+
+  getVideos(){
+
+    const url = `${ this.youtubeUrl }/playlistItems`
+
+    const params = new HttpParams()
+    .set('part', 'snippet')
+    .set('maxResults', '10')
+    .set('playlistId', this.playlist)
+    .set('key', this.apiKey)
+    .set('pageToken', this.nextToken)
+
+    return this.http.get<YoutubeResponse>(url, { params })
+      .pipe(
+        map( res => {
+          this.nextToken = res.nextPageToken;
+          return res.items;
+        }),
+        map( items => items.map( video => video.snippet )
+        )
+      );
+  }
 }
